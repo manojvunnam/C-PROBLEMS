8db3618b7c8d5864f8496228040cb2c086823e74
VUNNAM MANOJ <manojvunnam6@gmail.com>
2026-01-07 11:39:10 +0530

    Add files via upload

---
+++ 41-75.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int n,i,arr[5];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);}
+        int count=0;
+        for(i=0;i<n;i++){
+            if(arr[i]>=0){
+                count++;
+            }
+        }
+    printf("%d",count);
+    return 0;
+}
