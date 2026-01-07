40724d93aab3a6a88c0c6840e037f1c30989b4a1
VUNNAM MANOJ <manojvunnam6@gmail.com>
2026-01-07 10:58:31 +0530

    Add files via upload

---
+++ 34-75.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n,i,arr[100],min;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    }
+    min=arr[0];
+    for(i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("min:%d",min);
+    return 0;
+}
